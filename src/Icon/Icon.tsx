30c7f6af7423b0c15a83bb520821942be95f16b2
Zunaib Imtiaz <zunaib60@gmail.com>
2020-01-19 23:41:34 +0500

    Updated Styles Vasty. Major Break

--- src/Icon/Icon.tsx
+++ src/Icon/Icon.tsx
@@ -1,16 +1,30 @@
 import clsx from 'clsx';
 import React, { forwardRef } from 'react';
 
+export type IconVariant = 'default' | 'primary' | 'secondary' | 'info' | 'success' | 'warning' | 'danger' | 'white';
+
+export type IconSize = 'xs' | 'sm' | 'md' | 'lg';
+
 export interface IconProps {
   title?: string;
   className?: string;
   children: string;
+  /**
+ * To apply variant to Button
+ **/
+  variant?: IconVariant;
+  /**
+   * To apply size to Button
+   **/
+  size?: IconSize;
   onClick?: () => void;
 }
 
-export const Icon = forwardRef<HTMLDivElement, IconProps>(function({ title, className, children, onClick }, ref) {
+export const Icon = forwardRef<HTMLDivElement, IconProps>(function ({ title, className, children, onClick, variant, size }, ref) {
   return (
-    <i title={title} onClick={onClick} className={clsx('material-icons', className)} ref={ref}>
+    <i title={title} onClick={onClick}
+      className={clsx('material-icons', className, `icon-${variant}`, { [`icon-${size}`]: size })}
+      ref={ref}>
       {children}
     </i>
   );
