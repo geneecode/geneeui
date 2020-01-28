1d7cd200df4fa0677335f627e14af26630794f16
Zunaib Imtiaz <zunaib60@gmail.com>
2020-01-28 11:42:41 +0500

    Added Test Project

---
+++ examples/geneeui-test/README.md
@@ -0,0 +1,1 @@
+# Inside Project for Testing GeneeUI
---
+++ examples/geneeui-test/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'geneeui/lib/Button';
+
+
+const App: React.FC = () => {
+
+  return (
+      <Button size="md" variant="primary" >asd</Button>
+    
+  );
+}
+
+export default App;
